774abb3ef674a24e9d0633a2ae0ac49467f3e116
Stas Moroz <108823770+Morozik17@users.noreply.github.com>
2023-08-04 10:52:45 +0200

    dz

--- js/task-2.js
+++ js/task-2.js
@@ -6,5 +6,21 @@ const btnEl = document.querySelector(".button");
 // 2
 
 const imgEl = document.querySelector(".img");
-console.log(imgEl)
-imgEl.src = "https://images.squarespace-cdn.com/content/v1/51f17990e4b0bef09d514799/1512679153861-LA2E5A4XV0VMCMWDA0QN/bartlett.jpg?format=1500w"
+// imgEl.src = "https://images.squarespace-cdn.com/content/v1/51f17990e4b0bef09d514799/1512679153861-LA2E5A4XV0VMCMWDA0QN/bartlett.jpg?format=1500w"
+// console.log(imgEl)
+
+// 3
+
+const linkEl = document.querySelector(".link");
+// linkEl.href = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/800px-Banana-Single.jpg"
+// console.log(linkEl.href);
+
+const img2El = document.querySelector(".img2");
+// img2El.alt = "newAlt"
+// console.log(img2El);
+
+// 4
+const listEl = document.querySelector(".list");
+// const firstItem = listEl.firstElementChild
+// firstItem.textContent = "sadasd"
+// console.log(firstItem?.textContent)
