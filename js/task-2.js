e2bdb96f077338fc8fae10e17be7acbd2835a3bc
Stas Moroz <108823770+Morozik17@users.noreply.github.com>
2023-08-04 10:31:00 +0200

    commit

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+// 1
+const btnEl = document.querySelector(".button");
+// btnEl.textContent = "нове імя";
+// console.log(btnEl.textContent);
+
+// 2
+
+const imgEl = document.querySelector(".img");
+console.log(imgEl)
+imgEl.src = "https://images.squarespace-cdn.com/content/v1/51f17990e4b0bef09d514799/1512679153861-LA2E5A4XV0VMCMWDA0QN/bartlett.jpg?format=1500w"
